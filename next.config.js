dddfb097cae2aba9db341e1a3507dfc90a829933
Yuvraj Singh <yuvi4695@gmail.com>
2025-07-05 18:43:51 +0530

    fix: converted next.config.ts to js for vercel deploy

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com", "tailwindcss.com", "cdn.dummyjson.com", "dummyjson.com"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
